fcacc9b17122db9b82ba7bf6d5d460140abc9543
asimarshad11-a11y <asim.arshad11@gmail.com>
2026-08-27 09:40:12 +0100

    Make 1080 Solutions the primary public brand

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,10 +1,12 @@
 export const SITE_NAME = "1080 Solutions";
-export const SITE_LEGAL_NAME = "Algo AV Limited";
+export const SITE_MARKETING_ENTITY = "1080 Solutions Ltd";
+export const SITE_PARENT_COMPANY = "Algo AV Limited";
 export const SITE_URL = "https://www.1080solutions.co.uk";
 export const PHONE_DISPLAY = "0800 772 0012";
 export const PHONE_TEL = "tel:08007720012";
 export const EMAIL = "sales@1080solutions.co.uk";
-export const COMPANY_NUMBER = "SC824474";
+export const COMPANY_NUMBER = "SC566054";
+export const PARENT_COMPANY_NUMBER = "SC824474";
 export const COMPANY_JURISDICTION = "Scotland";
 export const FACEBOOK_URL = "https://www.facebook.com/1080cctv/";
 export const INSTAGRAM_URL = "https://www.instagram.com/1080solutionsltd/";
